c824a149225f0e7ac2b13ce9681b8c608eecc657
codingjlu <78052093+codingjlu@users.noreply.github.com>
2022-01-16 19:53:45 -0800

    fix webpack common bug

--- client/webpack.common.js
+++ client/webpack.common.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src/index.js')
+  entry: path.resolve(__dirname, 'src/index.js'),
   module: {
     rules: [
       {
